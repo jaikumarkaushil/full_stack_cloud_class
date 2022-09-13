332ff80c7b197df325762a7df67f78842a9b30da
jaikumarkaushil <jaikumarkaushil@gmail.com>
2022-09-13 19:51:14 -0400

    first commit

---
+++ Lec_1_Class_Excercise.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter your first input: "))
+num2 = int(input("Enter your second input: "))
+
+sub = num1 - num2
+add = num1 + num2
+mul = num1 * num2
+power = num1 ** num2
+div = num1 / num2
+
+print("Subtraction is: ",sub)
+print("Addition is: ",add)
+print("Multiplication is: ",mul)
+print("power is:",power)
+print("division is: ",div)
